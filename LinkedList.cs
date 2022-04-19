5c7d801aa059b62c5bc61745d43b7e5b5b8eb35c
saikiranreddy <saikiranreddy@gmail.com>
2022-04-20 01:07:10 +0530

    [kiran]-Added Simple Linked List

---
+++ LinkedList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class LinkedList
+    {
+        //Instance
+        internal Node head;
+
+        //Adding Data to LinkedList
+        internal void Add(int data)
+        {
+            //Object of Node
+            Node node = new Node(data);
+
+            //Add Data
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+
+            //Display LinkedList Data
+            Console.WriteLine("{0} inserted into the Linked List", node.data);
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Message
+            Console.WriteLine("Welcome To The Linked List Program");
+
+            //Calling Class LinkedList
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+
+            Console.ReadLine();
+        }
+    }
+}
